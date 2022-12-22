f67a0f8db23de1af0a14174464cab946c8ca9dfd
BKHlovesroblox <116910358+BKHlovesroblox@users.noreply.github.com>
2022-12-22 14:21:22 +0000

    4

--- platformer.py
+++ platformer.py
@@ -1,9 +1,14 @@
 import pygame
 
+# constant variables
+SCREEN_SIZE = (700,500)
+DARK_GREY = (50,50,50)
+
 #init
 pygame.init()
-screen = pygame.display.set_mode( (700,500) )
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
+while True:
 # game loop
 
     # imput
@@ -11,7 +16,8 @@ screen = pygame.display.set_mode( (700,500) )
     # update
 
     # draw
-    screen.fill( (r,g,b) )
+    screen.fill(DARK_GREY)
+    pygame.display.flip()
 
 # quit
 pygame.quit()
