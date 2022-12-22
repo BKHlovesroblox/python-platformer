ff24f4c0fc6b7ca38239256cc0451e92f5b9b74c
BKHlovesroblox <116910358+BKHlovesroblox@users.noreply.github.com>
2022-12-22 13:53:01 +0000

    3

--- platformer.py
+++ platformer.py
@@ -1,8 +1,17 @@
 import pygame
 
 #init
+pygame.init()
+screen = pygame.display.set_mode( (700,500) )
 
 # game loop
 
     # imput
+
     # update
+
+    # draw
+    screen.fill( (r,g,b) )
+
+# quit
+pygame.quit()
